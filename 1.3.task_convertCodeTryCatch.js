b94455ba71dcfb40d1c77442b82afffb43d1f999
Arshak Varshamyan <arshakvar81@gmail.com>
2023-03-23 21:48:48 +0400

    feat(throw new error)map is not a function

---
+++ 1.3.task_convertCodeTryCatch.js
@@ -0,0 +1,12 @@
+function mapArray(arr) {
+    try {
+      if (Array.isArray(arr)) {
+        return arr.map((item) => item * 2);
+      } else {
+        throw new Error("map is not a function");
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+  console.log(mapArray(5));
